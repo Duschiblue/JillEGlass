738f2cafc5c3883942a3dd645d491cec3a220658
Lena Elkins <caspercatwolf@gmail.com>
2023-02-21 15:33:18 +0100

    First

---
+++ src/App.vue
@@ -0,0 +1,155 @@
+<!-- Javascript -->
+<script>
+// Imports
+import News from "./components/News.vue"
+
+export default {
+  components: {
+    News
+  },
+
+  // Shopping Cart Hovering
+  data() {
+    return {
+      img: "../src/assets/shopping-cart.png"
+    }
+  },
+  methods: {
+    changecart() {
+      this.img = '../src/assets/sc1.png'
+    },
+    changecart1() {
+      this.img = '../src/assets/shopping-cart.png'
+    }
+  },
+}
+</script>
+
+<!-- ------------------------------------------------ -->
+
+<!-- HTML -->
+<template>
+  <!-- Website Title -->
+  <title>JillEGlass</title>
+
+  <!-- Topbar -->
+  <div class="topbar">
+    <img class="logo" src="../src/assets/jillelogo.png">
+    <a href="shop" onclick="" >Shop</a>
+    <a href="gallery">Gallery</a>
+    <a href="about">About</a>
+    <a href="contact">Contact</a>
+    <a href="#"><img class="cart" :src="img" @mouseover="changecart" @mouseout="changecart1"></a>
+  </div>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <a class="all" href="all">All</a>
+    <a href="sealife">Sealife</a>
+    <a href="hearts">Hearts</a>
+    <a href="holiday">Holiday</a>
+    <a href="other">Other</a>
+    <a href="misc">Misc</a>
+    <div class="spacer"/>
+    <News/>
+  </div>
+</template>
+
+<!-- ------------------------------------------------ -->
+
+<!-- CSS (Scoped) -->
+<style scoped>
+/* Imports */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400&family=Orbitron&family=Playfair+Display&family=Special+Elite&display=swap');
+
+/* Logo */
+.logo {
+  height: 40px;
+  margin-top: 5px;
+  vertical-align: middle;
+}
+
+/* Cart */
+.cart {
+  height: 30px;
+  margin-top: 10px;
+  vertical-align: middle;
+  float: right;
+  margin-right: 20px;
+  background: none;
+}
+
+/* Topbar Design */
+.topbar {
+  background-color: #679289;
+  height: 50px;
+  z-index: 9999;
+}
+
+/* Topbar Elements */
+.topbar a {
+  color: azure;
+  padding: 14px 16px;
+  text-decoration: none;
+  vertical-align: middle;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.all {
+  margin-top: 14px;
+}
+
+.topbar a:hover {
+  color: black;
+}
+
+/* Sidebar Design */
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  background-color: #9bccc1;
+  height: calc(100% - 50px);
+  width: 120px;
+  position: fixed;
+  z-index: -99;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  padding-top: 50px;
+  left: -90px;
+  transition: 0.3s;
+}
+
+.sidebar:hover {
+  left: 0;
+}
+
+/* Sidebar Elements */
+.sidebar a {
+  padding: 12px 2px 10px;
+  text-decoration: none;
+  font-family: 'Montserrat', sans-serif;
+  text-align: center;
+  font-size: 16px;
+  display: block;
+  color: black;
+}
+
+.sidebar a:hover {
+  background-color: #ade6d9;
+  color: black;
+  box-shadow: 3px 3px 5px #679289 inset;
+  transition: 0.15s;
+}
+
+.news {
+  margin-bottom: 14px;
+}
+
+.spacer {
+  flex: 1
+}
+
+</style>
+
+<!-- ------------------------------------------------ -->
---
+++ src/components/News.vue
@@ -0,0 +1,82 @@
+<!-- Javascript -->
+<script>
+export default {
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggle() {
+     this.open = !this.open
+    },
+  },
+}
+</script>
+
+<!-- ------------------------------------------------ -->
+
+<!-- HTML -->
+<template>
+    <!-- Subscribe Popup -->
+    <div v-on:click="toggle" class="news">News</div>
+    <div v-if="open">
+    <div v-on:click="toggle" class="backdrop" />
+        <div class="subscribe-container">
+            Placeholder
+        </div>
+    </div>
+</template>
+
+<!-- ------------------------------------------------ -->
+
+<!-- CSS -->
+<style>
+/* Background Darkens */
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 3;
+}
+
+/* Subscribe Button */
+.news {
+    padding: 12px 2px 10px;
+    text-decoration: none;
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    font-size: 16px;
+    display: block;
+    color: black;
+    transition: all 0.15s;
+    margin-bottom: 14px;
+}
+
+.news:hover {
+    background-color: #ade6d9;
+    color: black;
+    box-shadow: 3px 3px 5px #679289 inset;
+}
+
+/* Subscribe Input */
+.subscribe-container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    height: 400px;
+    display: flex;
+    width: 500px;
+    z-index: 5;
+}
+</style>
+
+<!-- ------------------------------------------------ -->
