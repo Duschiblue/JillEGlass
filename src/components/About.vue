4424875561b5101e19164abf35e8e98e0ad18f06
Lena Elkins <caspercatwolf@gmail.com>
2023-03-21 16:23:56 +0100

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,21 +10,27 @@
         <h1>About The Artist</h1>
         <div class="information-container">
 
-        <!-- Information & Images -->    
-        <h2>About The Glass</h2>
+            <!-- Information -->    
             <div class="row">
-                <p>All my pieces are made with borosilicate glass (hard glass) and are handsculpted in front of a flame at a few thousand fahrenheit degrees. No use of molds and very minimal use of tools.
-                    Starting off with solid glass rods, I shape the glass in the flame by using gathering-, pushing- and pulling techniques to get desired shape. The pieces then rest in a kiln to prevent stress in glass.</p>
-                <h2>About Me</h2>
-                <p>I was born and raised in Småland, Sweden, very close to the "Glass Kingdom" of Sweden (Orrefors, Kosta Boda and many others).
-                    Often I would take trips there, either shool fieldtrips or with family and friends. Glass has always been a big fascination to me.</p>
-                <p>I started my lampworking career August 1998 on Hawaii, where I learned from the very talented glass artists at Maui Crystal in Lahaina, Maui.</p>
-                <p>Working with glass was such an amazing feeling. I was more creative with drawing (mostly portraits by requests) and sculpting with clay while living in Sweden. The flow feeling of working hot glass had such an impact on me. I became addicted right away.</p>
-                <p>I met my husband on Maui and in 1999 I moved to the North Shore, Oahu where he lived. There I continued as an apprentice for Krista Woodward at Oceans in Glass, Haleiwa. Another talented artist to learn from.</p>
-                <p>2001 a move took us to Traverse City, Michigan and I started my own studio in October of that year. After four years there, another move took us to Mobile, Alabama on the Gulf coast where we lived for 7 1/2 years.
-                    Fall of 2013 I started my studio in charming Västervik on the east coast of Sweden, right by it's amazing archipelago. Here we live today with our two children</p>
-                <div id="lampworkimage">
-                    <img src="../assets/lampwork.png">
+                <div id="grid">
+                    <div class="text">
+                        <h2>About The Glass</h2>
+                        <p>All my pieces are made with borosilicate glass (hard glass) and are handsculpted in front of a flame at a few thousand fahrenheit degrees. No use of molds and very minimal use of tools.
+                            Starting off with solid glass rods, I shape the glass in the flame by using gathering-, pushing- and pulling techniques to get desired shape. The pieces then rest in a kiln to prevent stress in glass.</p>
+                        <h2>About Me</h2>
+                        <p>I was born and raised in Småland, Sweden, very close to the "Glass Kingdom" of Sweden (Orrefors, Kosta Boda and many others).
+                            Often I would take trips there, either shool fieldtrips or with family and friends. Glass has always been a big fascination to me.</p>
+                        <p>I started my lampworking career August 1998 on Hawaii, where I learned from the very talented glass artists at Maui Crystal in Lahaina, Maui.</p>
+                        <p>Working with glass was such an amazing feeling. I was more creative with drawing (mostly portraits by requests) and sculpting with clay while living in Sweden. The flow feeling of working hot glass had such an impact on me. I became addicted right away.</p>
+                        <p>I met my husband on Maui and in 1999 I moved to the North Shore, Oahu where he lived. There I continued as an apprentice for Krista Woodward at Oceans in Glass, Haleiwa. Another talented artist to learn from.</p>
+                        <p>2001 a move took us to Traverse City, Michigan and I started my own studio in October of that year. After four years there, another move took us to Mobile, Alabama on the Gulf coast where we lived for 7 1/2 years.
+                            Fall of 2013 I started my studio in charming Västervik on the east coast of Sweden, right by it's amazing archipelago. Here we live today with our two children</p>
+                    </div>
+
+                    <!-- Image -->
+                    <div id="lampworkimage">
+                        <img src="../assets/lampwork.png">
+                    </div>
                 </div>
             </div>
         </div>
@@ -48,7 +54,6 @@
     padding: 40px;
     display: flex;
     flex-direction: column;
-    
 }
 
 .row{
@@ -70,6 +75,11 @@ h2 {
     font-weight: 400;
     margin-top: 20px;
     width: 100%;
+}
+
+#grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 }
 
 p {
--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -8,35 +8,61 @@
     <!-- About Background -->
     <div class="main">
         <div class="itemcontainer">
+
+            <!-- Item 1 = Dolphin -->
             <div class="item1">
                 <div class="imagesize" ><img id="images" src="../assets/dolphin1.jpg"></div>
                 <h1>Dolphin</h1>
                 <p>Lampwork Boro Glass Pendant, Aqua Blue</p>
                 <a id="link1" href="https://www.etsy.com/se-en/listing/1400373163/lampwork-boro-glass-pendant-focal-bead?click_key=d2d393d29513c73d298e60dda670f1c38c500b0f%3A1400373163&click_sum=d6a3ed97&ref=shop_home_recs_1">BUY</a>
             </div>
+
+            <!-- Item 2 = Whale -->
             <div class="item1">
                 <div class="imagesize" ><img id="images" src="../assets/val1.jpg"></div>
                 <h1>Humpback Whale</h1>
                 <p>Lampwork Boro Glass Pendant, Aqua Purple</p>
                 <a id="link1" href="https://www.etsy.com/se-en/listing/1304263492/lampwork-boro-glass-pendant-focal-bead?click_key=e49569f452c3d4700c00b5056b3c7e4c91dc8ad3%3A1304263492&click_sum=707867c5&ref=shop_home_recs_2&cns=1">BUY</a>
             </div>
+
+            <!-- Item 3 = Focal Bead -->
             <div class="item1">
                 <div class="imagesize" ><img id="images" src="../assets/swirl1.jpg"></div>
                 <h1>Focal Bead</h1>
                 <p>Lampwork Boro Glass Pendant, Turquoise Blue</p>
                 <a id="link1" href="https://www.etsy.com/se-en/listing/1436946753/lampwork-boro-glass-pendant-focal-bead?click_key=7845a0f7f471db0288694c81d592371c8982d66c%3A1436946753&click_sum=0bfbf191&ref=shop_home_recs_3">BUY</a>
             </div>
+
+            <!-- Item 4 = Whale -->
             <div class="item1">
                 <div class="imagesize" ><img id="images" src="../assets/val2.jpg"></div>
                 <h1>Humpback Whale</h1>
                 <p>Lampwork Boro Glass Pendant, Turquoise Blue</p>
                 <a id="link1" href="https://www.etsy.com/se-en/listing/1298309182/lampwork-boro-glass-pendant-focal-bead?click_key=2079269e5fab04becf3bc77dabc53e5a4480ae2a%3A1298309182&click_sum=032b7a03&ref=shop_home_recs_4">BUY</a>
             </div>
+
+            <!-- Item 4 = Seahorse -->
             <div class="item1">
                 <div class="imagesize" ><img id="images" src="../assets/seahorse1.jpg"></div>
                 <h1>Seahorse</h1>
                 <p>Lampwork Boro Glass Pendant, Blue Green</p>
                 <a id="link1" href="https://www.etsy.com/se-en/listing/1433026667/lampwork-boro-glass-pendant-focal-bead?click_key=fcd1e00b94b3cae779f53bf7a92c9e3f5710df56%3A1433026667&click_sum=9c6a75ba&ref=shop_home_recs_5&cns=1">BUY</a>
+            </div>
+
+            <!-- Item 5 = Seahorse -->
+            <div class="item1">
+                <div class="imagesize" ><img id="images" src="../assets/seahorse2.jpg"></div>
+                <h1>Seahorse</h1>
+                <p>Lampwork Boro Glass Pendant, Black White</p>
+                <a id="link1" href="https://www.etsy.com/se-en/listing/1403244651/lampwork-boro-glass-pendant-focal-bead?click_key=e276d66f24656ec16bbad297b41404ef1c869387%3A1403244651&click_sum=8e1468c0&ref=shop_home_recs_6&cns=1">BUY</a>
+            </div>
+
+            <!-- Item 6 = Heart -->
+            <div class="item1">
+                <div class="imagesize" ><img id="images" src="../assets/heart1.jpg"></div>
+                <h1>Heart</h1>
+                <p>Lampwork Boro Glass Pendant, Black</p>
+                <a id="link1" href="https://www.etsy.com/se-en/listing/1425739309/lampwork-boro-glass-pendant-focal-bead?click_key=c5dadec5550e6e9593414657cbdb8f03502d333f%3A1425739309&click_sum=74b158c4&ref=shop_home_recs_7">BUY</a>
             </div>
         </div>
     </div>
